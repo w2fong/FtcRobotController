da308e7e6c1d647ff555867a758ee0d359c297b4
Richard Fong <richard.fong@farohealth.com>
2025-07-24 21:14:25 -0700

    Initial commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FirstAuto.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Autonomous(name = "First_Autonomous")
+public class FirstAuto  extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        //Initialization Code
+        waitForStart();
+
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FirstTeleop.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+public class FirstTeleop extends OpMode {
+    @Override
+    public void init() {
+        //Happens repeatedly during init
+    }
+
+    @Override
+    public void init_loop() {
+        //Happens repeatedly during init
+    }
+
+    @Override
+    public void start() {
+        //Happens once after program start
+    }
+
+    @Override
+    public void loop() {
+        //Happens repeatedly during the program
+    }
+
+    @Override
+    public void stop() {
+        //Happens once after stop
+    }
+}
